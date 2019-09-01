69b8e9fa43f8d159322cd07499e868ee649071ea
anoecolazo <anoecolazo@gmail.com>
2019-09-01 16:06:13 -0500

    l

---
+++ calculador/calculador/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace calculador
+{
+    partial class calculadora
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtboton7 = new System.Windows.Forms.Button();
+            this.txtboton8 = new System.Windows.Forms.Button();
+            this.txtboton9 = new System.Windows.Forms.Button();
+            this.txtboton4 = new System.Windows.Forms.Button();
+            this.txtboton5 = new System.Windows.Forms.Button();
+            this.txtboton6 = new System.Windows.Forms.Button();
+            this.txtboton1 = new System.Windows.Forms.Button();
+            this.txtboton2 = new System.Windows.Forms.Button();
+            this.txtboton3 = new System.Windows.Forms.Button();
+            this.txtbotonde0 = new System.Windows.Forms.Button();
+            this.txtdeunpunto = new System.Windows.Forms.Button();
+            this.txtesc = new System.Windows.Forms.Button();
+            this.txtesce = new System.Windows.Forms.Button();
+            this.txtdiagonal = new System.Windows.Forms.Button();
+            this.txtmenos = new System.Windows.Forms.Button();
+            this.txtpor = new System.Windows.Forms.Button();
+            this.txtmas = new System.Windows.Forms.Button();
+            this.txtigual = new System.Windows.Forms.Button();
+            this.txtporciernto = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Location = new System.Drawing.Point(130, 57);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(512, 64);
+            this.textBox1.TabIndex = 0;
+            // 
+            // txtboton7
+            // 
+            this.txtboton7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboton7.Location = new System.Drawing.Point(88, 189);
+            this.txtboton7.Name = "txtboton7";
+            this.txtboton7.Size = new System.Drawing.Size(107, 71);
+            this.txtboton7.TabIndex = 1;
+            this.txtboton7.Text = "7";
+            this.txtboton7.UseVisualStyleBackColor = true;
+            // 
+            // txtboton8
+            // 
+            this.txtboton8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboton8.Location = new System.Drawing.Point(233, 189);
+            this.txtboton8.Name = "txtboton8";
+            this.txtboton8.Size = new System.Drawing.Size(109, 71);
+            this.txtboton8.TabIndex = 2;
+            this.txtboton8.Text = "8";
+            this.txtboton8.UseVisualStyleBackColor = true;
+            // 
+            // txtboton9
+            // 
+            this.txtboton9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboton9.Location = new System.Drawing.Point(361, 189);
+            this.txtboton9.Name = "txtboton9";
+            this.txtboton9.Size = new System.Drawing.Size(113, 71);
+            this.txtboton9.TabIndex = 3;
+            this.txtboton9.Text = "9";
+            this.txtboton9.UseVisualStyleBackColor = true;
+            // 
+            // txtboton4
+            // 
+            this.txtboton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboton4.Location = new System.Drawing.Point(88, 303);
+            this.txtboton4.Name = "txtboton4";
+            this.txtboton4.Size = new System.Drawing.Size(107, 64);
+            this.txtboton4.TabIndex = 4;
+            this.txtboton4.Text = "4";
+            this.txtboton4.UseVisualStyleBackColor = true;
+            // 
+            // txtboton5
+            // 
+            this.txtboton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboton5.Location = new System.Drawing.Point(233, 303);
+            this.txtboton5.Name = "txtboton5";
+            this.txtboton5.Size = new System.Drawing.Size(109, 64);
+            this.txtboton5.TabIndex = 5;
+            this.txtboton5.Text = "5";
+            this.txtboton5.UseVisualStyleBackColor = true;
+            // 
+            // txtboton6
+            // 
+            this.txtboton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboton6.Location = new System.Drawing.Point(361, 303);
+            this.txtboton6.Name = "txtboton6";
+            this.txtboton6.Size = new System.Drawing.Size(113, 64);
+            this.txtboton6.TabIndex = 6;
+            this.txtboton6.Text = "6";
+            this.txtboton6.UseVisualStyleBackColor = true;
+            // 
+            // txtboton1
+            // 
+            this.txtboton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboton1.Location = new System.Drawing.Point(88, 411);
+            this.txtboton1.Name = "txtboton1";
+            this.txtboton1.Size = new System.Drawing.Size(107, 65);
+            this.txtboton1.TabIndex = 7;
+            this.txtboton1.Text = "1";
+            this.txtboton1.UseVisualStyleBackColor = true;
+            // 
+            // txtboton2
+            // 
+            this.txtboton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboton2.Location = new System.Drawing.Point(233, 411);
+            this.txtboton2.Name = "txtboton2";
+            this.txtboton2.Size = new System.Drawing.Size(109, 65);
+            this.txtboton2.TabIndex = 8;
+            this.txtboton2.Text = "2";
+            this.txtboton2.UseVisualStyleBackColor = true;
+            // 
+            // txtboton3
+            // 
+            this.txtboton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboton3.Location = new System.Drawing.Point(361, 411);
+            this.txtboton3.Name = "txtboton3";
+            this.txtboton3.Size = new System.Drawing.Size(113, 65);
+            this.txtboton3.TabIndex = 9;
+            this.txtboton3.Text = "3";
+            this.txtboton3.UseVisualStyleBackColor = true;
+            // 
+            // txtbotonde0
+            // 
+            this.txtbotonde0.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbotonde0.Location = new System.Drawing.Point(88, 524);
+            this.txtbotonde0.Name = "txtbotonde0";
+            this.txtbotonde0.Size = new System.Drawing.Size(254, 82);
+            this.txtbotonde0.TabIndex = 10;
+            this.txtbotonde0.Text = "0";
+            this.txtbotonde0.UseVisualStyleBackColor = true;
+            // 
+            // txtdeunpunto
+            // 
+            this.txtdeunpunto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdeunpunto.Location = new System.Drawing.Point(361, 524);
+            this.txtdeunpunto.Name = "txtdeunpunto";
+            this.txtdeunpunto.Size = new System.Drawing.Size(113, 71);
+            this.txtdeunpunto.TabIndex = 11;
+            this.txtdeunpunto.Text = ".";
+            this.txtdeunpunto.UseVisualStyleBackColor = true;
+            // 
+            // txtesc
+            // 
+            this.txtesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtesc.Location = new System.Drawing.Point(558, 189);
+            this.txtesc.Name = "txtesc";
+            this.txtesc.Size = new System.Drawing.Size(101, 81);
+            this.txtesc.TabIndex = 12;
+            this.txtesc.Text = "C";
+            this.txtesc.UseVisualStyleBackColor = true;
+            // 
+            // txtesce
+            // 
+            this.txtesce.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtesce.Location = new System.Drawing.Point(691, 189);
+            this.txtesce.Name = "txtesce";
+            this.txtesce.Size = new System.Drawing.Size(97, 81);
+            this.txtesce.TabIndex = 13;
+            this.txtesce.Text = "CE";
+            this.txtesce.UseVisualStyleBackColor = true;
+            // 
+            // txtdiagonal
+            // 
+            this.txtdiagonal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdiagonal.Location = new System.Drawing.Point(557, 292);
+            this.txtdiagonal.Name = "txtdiagonal";
+            this.txtdiagonal.Size = new System.Drawing.Size(102, 75);
+            this.txtdiagonal.TabIndex = 14;
+            this.txtdiagonal.Text = "/";
+            this.txtdiagonal.UseVisualStyleBackColor = true;
+            // 
+            // txtmenos
+            // 
+            this.txtmenos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmenos.Location = new System.Drawing.Point(691, 292);
+            this.txtmenos.Name = "txtmenos";
+            this.txtmenos.Size = new System.Drawing.Size(97, 75);
+            this.txtmenos.TabIndex = 15;
+            this.txtmenos.Text = "-";
+            this.txtmenos.UseVisualStyleBackColor = true;
+            // 
+            // txtpor
+            // 
+            this.txtpor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpor.Location = new System.Drawing.Point(559, 395);
+            this.txtpor.Name = "txtpor";
+            this.txtpor.Size = new System.Drawing.Size(100, 81);
+            this.txtpor.TabIndex = 16;
+            this.txtpor.Text = "X";
+            this.txtpor.UseVisualStyleBackColor = true;
+            // 
+            // txtmas
+            // 
+            this.txtmas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmas.Location = new System.Drawing.Point(686, 392);
+            this.txtmas.Name = "txtmas";
+            this.txtmas.Size = new System.Drawing.Size(102, 84);
+            this.txtmas.TabIndex = 17;
+            this.txtmas.Text = "+";
+            this.txtmas.UseVisualStyleBackColor = true;
+            // 
+            // txtigual
+            // 
+            this.txtigual.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtigual.Location = new System.Drawing.Point(556, 524);
+            this.txtigual.Name = "txtigual";
+            this.txtigual.Size = new System.Drawing.Size(103, 71);
+            this.txtigual.TabIndex = 18;
+            this.txtigual.Text = "=";
+            this.txtigual.UseVisualStyleBackColor = true;
+            // 
+            // txtporciernto
+            // 
+            this.txtporciernto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtporciernto.Location = new System.Drawing.Point(691, 524);
+            this.txtporciernto.Name = "txtporciernto";
+            this.txtporciernto.Size = new System.Drawing.Size(97, 71);
+            this.txtporciernto.TabIndex = 19;
+            this.txtporciernto.Text = "%";
+            this.txtporciernto.UseVisualStyleBackColor = true;
+            // 
+            // calculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 705);
+            this.Controls.Add(this.txtporciernto);
+            this.Controls.Add(this.txtigual);
+            this.Controls.Add(this.txtmas);
+            this.Controls.Add(this.txtpor);
+            this.Controls.Add(this.txtmenos);
+            this.Controls.Add(this.txtdiagonal);
+            this.Controls.Add(this.txtesce);
+            this.Controls.Add(this.txtesc);
+            this.Controls.Add(this.txtdeunpunto);
+            this.Controls.Add(this.txtbotonde0);
+            this.Controls.Add(this.txtboton3);
+            this.Controls.Add(this.txtboton2);
+            this.Controls.Add(this.txtboton1);
+            this.Controls.Add(this.txtboton6);
+            this.Controls.Add(this.txtboton5);
+            this.Controls.Add(this.txtboton4);
+            this.Controls.Add(this.txtboton9);
+            this.Controls.Add(this.txtboton8);
+            this.Controls.Add(this.txtboton7);
+            this.Controls.Add(this.textBox1);
+            this.Name = "calculadora";
+            this.Text = "calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button txtboton7;
+        private System.Windows.Forms.Button txtboton8;
+        private System.Windows.Forms.Button txtboton9;
+        private System.Windows.Forms.Button txtboton4;
+        private System.Windows.Forms.Button txtboton5;
+        private System.Windows.Forms.Button txtboton6;
+        private System.Windows.Forms.Button txtboton1;
+        private System.Windows.Forms.Button txtboton2;
+        private System.Windows.Forms.Button txtboton3;
+        private System.Windows.Forms.Button txtbotonde0;
+        private System.Windows.Forms.Button txtdeunpunto;
+        private System.Windows.Forms.Button txtesc;
+        private System.Windows.Forms.Button txtesce;
+        private System.Windows.Forms.Button txtdiagonal;
+        private System.Windows.Forms.Button txtmenos;
+        private System.Windows.Forms.Button txtpor;
+        private System.Windows.Forms.Button txtmas;
+        private System.Windows.Forms.Button txtigual;
+        private System.Windows.Forms.Button txtporciernto;
+    }
+}
+
